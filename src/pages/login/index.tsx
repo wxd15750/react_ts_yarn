a8192e089deaed62ab36c19164cac75503aba571
wxd_code <w15755583882022@163.com>
2023-07-05 23:41:39 +0800

    登录页面静态部分完工

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,54 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // 导入login的样式
 import './login.scss'
-import { Button, Col, Form, Input, Row } from 'antd'
+import { Button, Form, Input} from 'antd'
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
 
 export default function Login() {
+
+  // 定义加载中的效果
+  const [loading,setLoading] = useState<boolean>(false)
 
 
   const onFinish = (values: any) => {
     console.log('Success:', values);
+    setLoading(true)
   };
 
   return (
     <div className='loginContainer'>
-      <Row>
-        <Col span={24}>
-          <Form
+      <div className='loginForm'>
+        <h1 className='title'>React+ts后台管理系统</h1>
+        <Form
             name="basic"
-            labelCol={{ span: 6}}
-            wrapperCol={{ span: 16 }}
-            initialValues={{ remember: true }}
+            // labelCol={{ span: 6}}
+            // wrapperCol={{ span: 16 }}
+            // initialValues={{ remember: true }}
             onFinish={onFinish}
             autoComplete="off"
           >
             <Form.Item
-              label="账号"
               name="username"
-              rules={[{ required: true, message: 'Please input your username!' }]}
+              
+              rules={[{ required: true, message: '请输入用户名' }]}
             >
-              <Input />
+              <Input placeholder='账号' prefix={<UserOutlined />} />
             </Form.Item>
 
             <Form.Item
-              label="密码"
               name="password"
-              rules={[{ required: true, message: 'Please input your password!' }]}
+              rules={[{ required: true, message: '请输入密码' }]}
             >
-              <Input.Password />
+              <Input.Password  placeholder='密码' prefix={<LockOutlined/>}/>
             </Form.Item>
-
-            
-
-            <Form.Item wrapperCol={{  span: 18 }}>
-              <Button type="primary" htmlType="submit" style={{width:"100%"}}>
+            <Form.Item >
+              <Button type="primary" loading={loading} htmlType="submit" className='btn'>
                 登录
               </Button>
             </Form.Item>
           </Form>
-        </Col>
-      </Row>
+       
+
+      </div>
+          
     </div>
   )
 }
