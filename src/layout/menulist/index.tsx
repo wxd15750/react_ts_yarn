9bbe7231e29bfc74653857bc17cec729edfeb9d3
wxd_code <w15755583882022@163.com>
2023-07-07 22:46:31 +0800

    tabber导航栏静态的搭建

--- src/layout/menulist/index.tsx
+++ src/layout/menulist/index.tsx
@@ -3,18 +3,27 @@ import React, { useState } from 'react'
 import {Layout, Menu } from 'antd';
 
 import {
-  MenuFoldOutlined,
+
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
 } from '@ant-design/icons';
 import Logo from '../logo';
 
+// 获取路由
+
 const { Sider } = Layout;
+
+
 
 export default function MenuList() {
 
   const [collapsed, setCollapsed] = useState(false);
+
+  const toPath = (e:any) => {
+    console.log(e);
+
+  }
   
   return (
     <Sider trigger={null} collapsible collapsed={collapsed}>
@@ -23,20 +32,21 @@ export default function MenuList() {
         <Menu
           theme="dark"
           mode="inline"
+          onClick={toPath}
           defaultSelectedKeys={['1']}
           items={[
             {
-              key: '1',
+              key: '/home',
               icon: <UserOutlined />,
               label: 'nav 1',
             },
             {
-              key: '2',
+              key: '/login',
               icon: <VideoCameraOutlined />,
               label: 'nav 2',
             },
             {
-              key: '3',
+              key: '/404',
               icon: <UploadOutlined />,
               label: 'nav 3',
             },
